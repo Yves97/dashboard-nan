31142e2569344a3a891d2d37ab02b83a7b5dbfe4
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-02-28 09:46:12 +0000

    nan admin1

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,4 +1,13 @@
 $('.sidebar-control').click(function(){
     $('.sidebar').animate({width: 'toggle'})
-    $('.sidebar-control img').attr("src","ressources/images/cancel.svg")
+    // if($('.sidebar-control img').attr("src","ressources/images/menu.svg")){
+    //     $('.sidebar-control img').attr("src","ressources/images/cancel.svg")
+    //     if($('.sidebar-control img').attr("src","ressources/images/cancel.svg")){
+    //         $('.sidebar-control img').attr("src","ressources/images/menu.svg")
+    //     }
+    // }
+    
+    // else{
+    //     $('.sidebar-control img').attr("src","ressources/images/menu.svg")
+    // }
 })
